cb535f5bb9b225be982c7fd629244bd3116a1608
Cal-Rex <107879632+Cal-Rex@users.noreply.github.com>
2023-12-14 21:39:55 +0000

    refine and test base js, get it working. (#23)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,65 @@
-console.log("JavaScript is connected!");
-
-
-
 $(document).ready(function() {
+    const CHEAPBUTTON = $('#row1-id').attr('value');
+    const CONTINENTTEST = $('#row2-id').attr('value');
     let giftGrade = '';
     let continent = '';
 
     function loadLandingPage() {
+        // makes sure landing page displays if page reloaded
+        // or back button pressed
         $('#landing').removeClass('display-none');
         $('#row1-id').addClass('display-none');
         $('#row2-id').addClass('display-none');
         $('#row3-id').addClass('display-none');
-        // make sure to add the following class to the css:
-        // .display-none {display: none;}
     }
 
     function loadPageOne() {
+        // makes sure page one loads on start button click
+        // or back button pressed
         $('#landing').addClass('display-none');
         $('#row1-id').removeClass('display-none');
         $('#row2-id').addClass('display-none');
         $('#row3-id').addClass('display-none');
-        // make sure to add the following class to the css:
-        // .display-none {display: none;}
     }
 
     function loadPageTwo() {
+        // makes sure page two loads on start button click
+        // or back button pressed
         $('#landing').addClass('display-none');
         $('#row1-id').addClass('display-none');
         $('#row2-id').removeClass('display-none');
         $('#row3-id').addClass('display-none');
-        // make sure to add the following class to the css:
-        // .display-none {display: none;}
     }
 
     function loadPageThree() {
+        // makes sure page three loads on start button click
+        // or back button pressed
         $('#landing').addClass('display-none');
         $('#row1-id').addClass('display-none');
         $('#row2-id').addClass('display-none');
         $('#row3-id').removeClass('display-none');
-        // make sure to add the following class to the css:
-        // .display-none {display: none;}
     }
 
-    function getGiftGrade(grade) {
-        // get the "value" attribute off button
-        // 3 buttons with 1 corresponding value
-        giftGrade = grade;
+    function getGiftGrade() {
+        // get the "value" attribute off the clicked gift-grade buttons
+        giftGrade = $(this).attr('value');
+        console.log("grade selected: " + giftGrade);
         loadPageTwo();
     };
 
-
-    function getContinent(selectedContinent) {
-        // get the "value" attribute off button
-        // 3 buttons with 1 corresponding value
-        continent = selectedContinent;
+    function getContinent() {
+        // get the "value" attribute off the clicked continent buttons
+        continent = $(this).attr('value');
+        console.log("continent selected: " + continent);
         loadPageThree();
     };
 
-    $('#start-button').addEventListener('click', loadPageOne);
-    $('.page-one-button').addEventListener('click', getGiftGrade);
-    $('.page-two-button').addEventListener('click', getContinent);
-    $('.page-one-back-button').addEventListener('click', loadLandingPage);
-    $('.page-two-back-button').addEventListener('click', loadPageOne);
-    $('.page-three-back-button').addEventListener('click', loadPageTwo);
+
+    $('#start-button').on('click', loadPageOne);
+    $('#page-one-button').on('click', getGiftGrade);
+    $('#page-two-button').on('click', getContinent);
+    $('#page-one-back-button').on('click', loadLandingPage);
+    $('#page-two-back-button').on('click', loadPageOne);
+    $('#page-three-back-button').on('click', loadPageTwo);
+
 });
